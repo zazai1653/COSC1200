bb4871436b90726f17b98d9f478584b4c081d2d2
zai <pristinevvs@gmail.com>
2025-05-22 12:26:05 -0400

    Commit 3 - start calc and validation
    Code to start calculation added, code for height input and validation also added.

--- A1/src/Main.java
+++ A1/src/Main.java
@@ -22,5 +22,38 @@ public class Main {
         boolean validationLoopForWeightInPounds = true;
         float userInputWeightInPounds = 0;
 
+        // Do while loop
+        while (loopForContinuingBmiCalculation) {
+
+            // Resetting all variables for every run of this do while loop
+            loopForContinuingBmiCalculation = true;
+            userInputStartBmiCalculation = 0;
+
+            validationLoopForHeightInInches = true;
+            userInputHeightInInches = 0;
+
+            validationLoopForWeightInPounds = true;
+            userInputWeightInPounds = 0;
+
+            System.out.println("Type 1 to calculate BMI or 2 to stop");
+
+            // Validation to get an int of 1 or 2
+            if (input.hasNextInt()) {
+                userInputStartBmiCalculation = input.nextInt();
+
+                //If valid float
+                if (userInputHeightInInches >= 24  && userInputHeightInInches <= 120){
+
+                }
+
+                else {
+                    System.out.println("Invalid input of " + userInputHeightInInches);
+                }
+            // User did not enter a float
+            else {
+                System.out.println("No int entered");
+                loopForContinuingBmiCalculation = true;
+                input.next();
+            }
     }
 }
