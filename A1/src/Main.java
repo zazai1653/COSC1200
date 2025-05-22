4422e1e44a6c16ec4612256244ff958145c466fd
zai <pristinevvs@gmail.com>
2025-05-22 12:19:13 -0400

    Commit 1 - A1 project created
    Assignment 1 project created, java file ready to complete.

---
+++ A1/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        final Scanner input = new Scanner(System.in);
+        int hi = input.nextInt();
+        System.out.println(hi);
+
+    }
+}
