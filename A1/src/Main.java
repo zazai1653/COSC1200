5307b39491eea4f4a07c9e6e5f5e2052a32b372c
zai <pristinevvs@gmail.com>
2025-05-22 12:44:47 -0400

    Commit 4 - Code fixes
    Fixed formatting to make code look cleaner, also fixed the height prompt which said enter height between 40 and 120, when it should have been 24 and 120. All code works to meet assignment functions.

--- A1/src/Main.java
+++ A1/src/Main.java
@@ -1,6 +1,5 @@
 import java.util.*;
 
-
 public class Main {
 
     public static void main(String[] args) {
@@ -24,7 +23,6 @@ public class Main {
 
         // Do while loop
         while (loopForContinuingBmiCalculation) {
-
             // Resetting all variables for every run of this do while loop
             loopForContinuingBmiCalculation = true;
             userInputStartBmiCalculation = 0;
@@ -37,23 +35,109 @@ public class Main {
 
             System.out.println("Type 1 to calculate BMI or 2 to stop");
 
-            // Validation to get an int of 1 or 2
+            // Validation to get an int
             if (input.hasNextInt()) {
                 userInputStartBmiCalculation = input.nextInt();
 
-                //If valid float
-                if (userInputHeightInInches >= 24  && userInputHeightInInches <= 120){
+                // User inputs a valid int of 1 or 2
+                if (userInputStartBmiCalculation == 1 || userInputStartBmiCalculation == 2) {
 
+                    // User wants to continue
+                    if (userInputStartBmiCalculation == 1) {
+
+                        // Do while loop that repeats in case of invalid input
+                        while (validationLoopForHeightInInches) {
+                            System.out.println("Enter height in inches, no less than 24.0 and no more than 120.0");
+
+                            if (input.hasNextFloat()) {
+                                userInputHeightInInches = input.nextFloat();
+
+                                //If valid float
+                                if (userInputHeightInInches >= 24  && userInputHeightInInches <= 120){
+
+                                    // Do while loop for weight
+                                    while (validationLoopForWeightInPounds){
+                                        System.out.println("Enter weight in pounds, no less than 25");
+
+                                        if (input.hasNextFloat()){
+                                            userInputWeightInPounds = input.nextFloat();
+
+                                            // User enters a float that is valid
+                                            if (userInputWeightInPounds >= 25){
+                                                // Get the BMI by using the emperical formula.
+                                                userBmi = (userInputWeightInPounds / (userInputHeightInInches * userInputHeightInInches)) * CONVERSION_FACTOR;
+
+                                                System.out.printf("The height you entered is %.1f, and the weight is %.1f, making your BMI %.1f%n", userInputHeightInInches, userInputWeightInPounds, userBmi);
+
+                                                // If structure for all BMIs that may suggest weight loss/gain.
+                                                if (userBmi < 16) {
+                                                    System.out.println("You are severely underweight and need to gain weight");
+                                                }
+                                                else if (userBmi >= 16 && userBmi < 18.5) {
+                                                    System.out.println("You are underweight and should gain weight");
+                                                }
+                                                else if (userBmi >= 18.5 && userBmi < 25) {
+                                                    System.out.println("You are healthy and do not need to change weight");
+                                                }
+                                                else if (userBmi >= 25 && userBmi < 30) {
+                                                    System.out.println("You are overweight and should lose weight");
+                                                }
+                                                else {
+                                                    System.out.println("You are obese and need to lose weight");
+                                                }
+
+                                                //Extra new line to improve the format of the new calculation
+                                                System.out.println(' ');
+
+                                                // Falsifying all loop conditions to go back to initial prompt
+                                                validationLoopForHeightInInches = false;
+                                                validationLoopForWeightInPounds = false;
+                                            }
+
+                                            // User enters a float that is valid
+                                            else {
+                                                System.out.println("Enter a value equal to or greater than 25");
+                                            }
+                                        }
+
+                                        else {
+                                            System.out.println("You didn't enter a float");
+                                        }
+                                    }
+                                }
+
+                                else {
+                                    System.out.println("Invalid input of " + userInputHeightInInches);
+                                }
+
+                            }
+
+                            //Not a float
+                            else{
+                                System.out.println("Enter a float type number");
+                            }
+                        }
+                    }
+
+                    // Valid input by user who wants to exit
+                    if (userInputStartBmiCalculation == 2) {
+                        break;
+                    }
                 }
 
+                // Valid float but input but not 1 or 2
                 else {
-                    System.out.println("Invalid input of " + userInputHeightInInches);
+                    System.out.println("Invalid int");
+                    loopForContinuingBmiCalculation = true;
                 }
+            }
+
             // User did not enter a float
             else {
                 System.out.println("No int entered");
                 loopForContinuingBmiCalculation = true;
                 input.next();
             }
+        }
     }
 }
