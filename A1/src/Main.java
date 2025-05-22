34762a76404fbdf7d251f804a8aa222cd24c76cb
zai <pristinevvs@gmail.com>
2025-05-22 12:21:08 -0400

    Commit 2 - variables added
    All variables needed for program have been added.

--- A1/src/Main.java
+++ A1/src/Main.java
@@ -5,8 +5,22 @@ public class Main {
 
     public static void main(String[] args) {
         final Scanner input = new Scanner(System.in);
-        int hi = input.nextInt();
-        System.out.println(hi);
+
+        // Used for getting the BMI
+        final float CONVERSION_FACTOR = 703;
+        float userBmi = 0;
+
+        // BMI calculation process variables
+        boolean loopForContinuingBmiCalculation = true;
+        int userInputStartBmiCalculation = 0;
+
+        // Height variables
+        boolean validationLoopForHeightInInches = true;
+        float userInputHeightInInches = 0;
+
+        // Weight variables
+        boolean validationLoopForWeightInPounds = true;
+        float userInputWeightInPounds = 0;
 
     }
 }
