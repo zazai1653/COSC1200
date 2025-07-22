6203c990ce56b6292efa509fef7a3df481e693f2
zai <pristinevvs@gmail.com>
2025-07-22 12:35:06 -0400

    A4 C3 - coded exit button and more
    Coded the exit button and added text wrapping I learned from a youtube video, I also added some labels to make the UI more understandable.

--- A4 - Simple Text Editor/untitled/src/Main.java
+++ A4 - Simple Text Editor/untitled/src/Main.java
@@ -9,12 +9,14 @@ public class Main {
         System.out.println("Hi");
     }
     public static void main(String[] args) {
-
         JFrame frame = new JFrame("Simple Text Editor");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
+        frame.setSize(800, 800);
         frame.setLocationRelativeTo(null);
         frame.setLayout(new BorderLayout());
+
+        JFileChooser fileChooser = new JFileChooser();
+        File currentFile = null;
 
         // greeting panel
         JPanel greetingPanel = new JPanel(new GridLayout(1, 1));
@@ -24,6 +26,12 @@ public class Main {
         JPanel buttonPanel = new JPanel(new GridLayout(1, 3, 5, 5));
 
         JButton openButton = new JButton("Open");
+        openButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+
+            }
+        });
         buttonPanel.add(openButton);
 
         JButton saveButton = new JButton("Save");
@@ -33,22 +41,31 @@ public class Main {
         exitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                frame.dispose(); // Close the frame
+                frame.dispose();
             }
         });
         buttonPanel.add(exitButton);
 
-        // button panel code above (Open, Save, Exit) code below
+        // JTextArea used to write and read to and from files
+        JTextArea textArea = new JTextArea();
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
+        JPanel textAreaPanel = new JPanel(new GridLayout(2,1));
+        textAreaPanel.add(new JLabel("In the text area below you can write what you'd like to the file, or see the contents of the file chosen."));
+        textAreaPanel.add(textArea);
 
         // wrapper panel with vertical BoxLayout
         JPanel wrapper = new JPanel();
         wrapper.setLayout(new BoxLayout(wrapper, BoxLayout.Y_AXIS));
         wrapper.add(greetingPanel);
         wrapper.add(buttonPanel);
+        wrapper.add(textAreaPanel);
+
+        // Secret sauce: fortune teller
 
         // add wrapper
         frame.add(wrapper);
-
         frame.setVisible(true);
     }
 }
