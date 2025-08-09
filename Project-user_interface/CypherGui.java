b49cd9af3cb0129012bdab7982fdcbb7a265fa5f
zai <pristinevvs@gmail.com>
2025-08-08 20:22:34 -0400

    A5C3 - adding buttons

--- Project-user_interface/CypherGui.java
+++ Project-user_interface/CypherGui.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.*;
 import java.util.*;
 
@@ -52,17 +53,73 @@ public class CypherGui {
         buttonGroup.add(translateButton);
         buttonGroup.add(substitutionButton);
 
-        // label 4/6 - shift key
-        JPanel shiftKeyPanel = new JPanel(new GridLayout(2, 1));
-        shiftKeyPanel.add(new JLabel("Enter the shift key you would like to use:"));
-        JTextField shiftKeyTextField = new JTextField();
-        shiftKeyPanel.add(shiftKeyTextField);
+        // label 4+5/6 - shift key and complexity key
+        JPanel shiftAndComplexityKeyPanel = new JPanel(new GridLayout(2, 2));
+        shiftAndComplexityKeyPanel.add(new JLabel("Enter the shift key you would like to use:"));
+        shiftAndComplexityKeyPanel.add(new JLabel("Enter the complexity key you would like to use:"));
 
-        // label 5/6 - complexity key
-        JPanel complexityKeyPanel = new JPanel(new GridLayout(2, 1));
-        complexityKeyPanel.add(new JLabel("Enter the complexity key you would like to use:"));
+        JTextField shiftKeyTextField = new JTextField();
+        shiftAndComplexityKeyPanel.add(shiftKeyTextField);
         JTextField complexityKeyTextField = new JTextField();
-        complexityKeyPanel.add(complexityKeyTextField);
+        shiftAndComplexityKeyPanel.add(complexityKeyTextField);
+
+        // 3 buttons of encrypt, decrypt and exit
+        JPanel buttonPanel = new JPanel(new FlowLayout (FlowLayout.CENTER, 10, 5));
+        JButton encryptButton = new JButton("Encrypt");
+        encryptButton.setToolTipText("Click/Alt+E to encrypt the text");
+        encryptButton.setMnemonic(KeyEvent.VK_E);
+        buttonPanel.add(encryptButton);
+
+        JButton decryptButton = new JButton("Decrypt");
+        decryptButton.setToolTipText("Click to decrypt the text");
+        buttonPanel.add(decryptButton);
+
+        JButton exitButton = new JButton("Exit");
+        exitButton.setToolTipText("Click to exit the GUI");
+        buttonPanel.add(encryptButton);
+
+        // Buttons logic
+        encryptButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent event){
+                System.out.println();
+            }
+        });
+
+        decryptButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent event){
+                System.out.println();
+            }
+        });
+
+        exitButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent event){
+                frame.dispose();
+            }
+        });
+
+        /*
+        // label 1/6 - instructions and errors panel and the text area
+        JPanel instructionsAndErrorsPanel = new JPanel(new GridLayout(2, 1));
+        JTextArea instructionsAndErrorsLabel = new JTextArea("Greetings! Welcome to the cipher application using a GUI. Write what you would like to process as a cipher below, choose the alphabet, the method to be used (substitution/translation), the shift and complexity key and then select whether you would like to encrypt or decrypt the message.");
+        instructionsAndErrorsLabel.setLineWrap(true);
+        instructionsAndErrorsLabel.setWrapStyleWord(true);
+        instructionsAndErrorsLabel.setFocusable(false);
+        instructionsAndErrorsLabel.setEditable(false);
+        instructionsAndErrorsPanel.add(instructionsAndErrorsLabel);
+
+        JTextArea userInput = new JTextArea();
+        userInput.setLineWrap(true);
+        userInput.setWrapStyleWord(true);
+        userInput.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        scrollPane = new JScrollPane(userInput);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        instructionsAndErrorsPanel.add(scrollPane);
+
+        */
+
 
         // Wrapper with vertical BoxLayout
         JPanel wrapper = new JPanel();
@@ -70,7 +127,7 @@ public class CypherGui {
         wrapper.add(instructionsAndErrorsPanel);
         wrapper.add(alphabetsPanel);
         wrapper.add(radioButtonsTranslateSubstitutePanel);
-        wrapper.add(shiftKeyPanel);
+        wrapper.add(shiftAndComplexityKeyPanel);
 
         frame.add(wrapper);
         frame.setVisible(true);
